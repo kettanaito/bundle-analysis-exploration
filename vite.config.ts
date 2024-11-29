cf74dab35277cfdbabc36db304694e85ade1c61e
Artem Zakharchenko <kettanaito@gmail.com>
2024-11-29 11:25:22 +0100

    install codecov remix-vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { codecovRemixVitePlugin } from '@codecov/remix-vite-plugin'
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: true
   }
 }
 
@@ -20,5 +21,10 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    codecovRemixVitePlugin({
+      enableBundleAnalysis: true,
+      bundleName: 'example-remix-bundle',
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
   ],
-});
+})
